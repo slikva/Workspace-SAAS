3062d8a2e227ab0688d31240c5361b7758bbafbd
Slikva <vasavislikva@gmail.com>
2026-06-25 17:41:20 +0530

    Ready for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "workspace_saas",
-  password: "Vasu1603",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "workspace.saas.ac@gmail.com",
-    pass: "kxpzfrlygfjmfyxs"
-  }
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+
 });
+
 
 app.use(express.json());
 app.use(cors());
@@ -1225,6 +1227,8 @@ app.get("/test-email", async (req, res) => {
 
 
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
