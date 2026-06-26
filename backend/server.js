fafca286e0608cdff14bf97fce7617fcc9447285
Slikva <vasavislikva@gmail.com>
2026-06-26 12:26:13 +0530

    Update email transporter

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const app = express();
 const nodemailer = require("nodemailer"); 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
