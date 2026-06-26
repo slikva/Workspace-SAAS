a529bde1df29deb39dad5628bba048849102c6f0
Slikva <vasavislikva@gmail.com>
2026-06-26 10:14:16 +0530

    Update SMTP config

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const app = express();
 const nodemailer = require("nodemailer"); 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
+  
 });
 
 
