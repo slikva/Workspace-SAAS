70b30b7840b1f6fb7cf4e39b034bc1b7ce4bc9b3
Vasavi Slikva <vasavislikva@gmail.com>
2026-07-03 15:38:27 +0530

    Update SMTP configuration for nodemailer

--- backend/server.js
+++ backend/server.js
@@ -42,12 +42,12 @@ app.use(express.json());
 app.use(cors());
 const nodemailer = require("nodemailer"); 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_SMTP_USER,
+    pass: process.env.BREVO_SMTP_KEY,
   },
 });
 transporter.verify((error, success) => {
